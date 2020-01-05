209ff42e9bcb654bf80bac11c69075b7c192490f
Dmitry Isaenko <isaenkodmitry@gmail.com>
2020-01-06 00:07:06 +0200

    Working with header

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -3,21 +3,48 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Скрытие/отображение пунктов меню для моб. версии
     var $toggleButton = $('.toggle-button'),
 		$navWrap = $('.nav-wrap');
-		$menuItemHasChildren = $('.menu-item-has-children');
 		
 		$toggleButton.on('click', function() {
             $(this).toggleClass('button-open');
             $navWrap.toggleClass(['menu-show', 'mobile']);
-            // $navWrap.toggleClass('mobile');
+			$addonPhonesBlock.toggleClass('show-contact-item-md-addon');
 		});
 
-		//Show sub-menu item in mobile version
-		$menuItemHasChildren.click(function() {
-			$menuItemHasChildren.not(this).children('.sub-menu').hide(300);
-			$(this).children('.sub-menu').toggle(300);
+	//Show sub-menu item in mobile version
+	// var $menuItemHasChildren = $('.menu-item-has-children');
+	var $menuItemHasChildren = $('.sidebar-menu-arrow');
+	$menuItemHasChildren.on('click', function() {
+		if ($(window).width() <= 660) {
+			$menuItemHasChildren.not(this).next('.sub-menu').hide(300);
+			$(this).next('.sub-menu').toggle(300);
+		}
+	});
+
+	//Show block with addon phones
+	var $addonPhonesBtn = $('.fa-chevron-circle-down');
+		// $addonPhonesBlock = $('.contact-item-md-addon');
+
+		$addonPhonesBtn.on('click', function(){
+			$('#modalCall').modal('show');
+			// $addonPhonesBlock.toggleClass('show-contact-item-md-addon');
 		});
 
-	
+	//Collapse main header to smaller height after scroll
+	$(window).scroll(function() {
+		if ( ( $(window).scrollTop() >= 100 ) && ($(window).width() > 991) ) {
+			$('.contacts, .header-lang').css('display', 'none');
+			$('.main-logo__img').addClass('main-logo__img_scroll');
+			$('.contact-item-scroll').addClass('contact-item-scroll_show');
+			$('.subheader').addClass('subheader_scroll').animate({'opacity':'1'},500);
+		} else {
+			$('.contacts, .header-lang').attr('style', ''); 
+			$('.main-logo__img').removeClass('main-logo__img_scroll'); 
+			$('.contact-item-scroll').removeClass('contact-item-scroll_show');
+			$('.subheader').removeClass('subheader_scroll');
+		}
+	});
+
+
 	// Плавный переход между разделами сайта при клике на пункты меню
 	$("#menu").on("click","a", function (event) {
 		event.preventDefault();
